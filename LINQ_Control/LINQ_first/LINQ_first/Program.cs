385568841e4fb48d3afe9a19e30419644f0d3b89
Dmitrii <Dimono30042000@mail.ru>
2019-03-26 13:24:29 +0300

    COntrol

---
+++ LINQ_Control/LINQ_first/LINQ_first/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_first
+{
+    class Program
+    {
+        static void GetAnswer(List<int> list)
+        {
+            int i = 0;
+            var newList = list
+                .Select(x =>
+            {
+                i++;
+                return x * i;                
+            })
+            .Where(x => x >= 10 && x <= 99)
+            .Reverse();
+            foreach (var e in newList)
+                Console.Write(e + " ");
+        }
+
+
+        static void Main(string[] args)
+        {
+            var list = new List<int> { 11, 12, 13, 345 };
+            GetAnswer(list);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LINQ_Control/LINQ_second/LINQ_second/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_second
+{
+    class Program
+    {
+        private static IEnumerable<Tuple<T, T>> GetTuples<T>( IEnumerable<T> item1, IEnumerable<T> item2, Func<T, T, bool> filter)
+        {
+            foreach (var e in item1)
+                foreach (var t in item2)
+                {
+                    if (filter(e, t))
+                        yield return Tuple.Create(e, t);
+                }
+        }
+    }
+}
