a83f4e541ce051d213399d92543ff1aa50df32b9
Dmitrii <Dimono30042000@mail.ru>
2019-05-14 22:32:53 +0300

    Control

---
+++ Control_Two/FirstNumberWithComments/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlTask_1
+{
+    // класс данных: имя, свойство, то есть значение, и метод
+    class MyClass
+    {
+        public string Name { get; set; }
+        public int Property { get; set; }
+        public int Method(int argument)
+        {
+            return Property + argument;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // путь до папки
+            string folder = @"C:\Users\Дмитрий\source\repos\ControlTask_1\ControlTask_1\folder";
+            // выводим все каталоги и папки
+            GetInformationAboutFilesAndDirectory(folder);
+            Console.WriteLine("\n");
+            // получаем результат
+            var variable = GetRightAnswer();
+            // считывает папки
+            var newFolders = Directory.GetDirectories(folder);
+            // количество правильных ответов
+            int count = 0;
+            foreach (var e in newFolders)
+            {
+                // берем файлы
+                var newFiles = Directory.GetFiles(e);
+                foreach (var s in newFiles)
+                {
+                    // считываем ответ и сравниваем с ответом в нашем классе
+                    var str = File.ReadAllLines(s);
+                    var answer = int.Parse(str[0]);
+                    if (variable == answer)
+                        count++;// увеличиваем счетчик
+                }
+                var information = new MyClass { Name = e, Property = count };
+                // заносим результат в таблицу, где отображается имя и количество верных ответов
+                Print(information);
+                // обнуляем счетчик
+                count = 0;
+            }
+            Console.ReadKey();
+        }
+        // заполняем тадлицу
+        static void Print(object obj)
+        {
+
+            var builder = new StringBuilder();
+            // заводим тип
+            var type = obj.GetType();
+            foreach (var property in type.GetProperties())
+            {
+                builder.Append(property.Name);
+                builder.Append(" : ");
+                builder.Append(property.GetValue(obj));
+                builder.Append("\n");
+            }
+            // выводим данные
+            var str = builder.ToString();
+            Console.WriteLine(str);
+        }
+        // вычисляем ответ
+        static int GetRightAnswer()
+        {
+            var type = typeof(MyClass);
+            ConstructorInfo ctor = type.GetConstructor(new Type[] { });
+            var result = ctor.Invoke(new object[] { });
+            // присваиваем значения
+            var propertyInfo = type.GetProperty("Property");
+            propertyInfo.SetValue(result, 5);
+            // считаем значение в методе
+            var argument = int.Parse(Console.ReadLine());
+            return (int)type.GetMethod("Method").Invoke(result, new object[] { argument });
+        }
+        // метод, отвечающий за вывод всех каталогов и файлов
+        static void GetInformationAboutFilesAndDirectory(string folder)
+        {
+            if (Directory.Exists(folder))
+            {
+                // выводим каталоги - названия папок
+                Console.WriteLine("Folders:");
+                var folders = Directory.GetDirectories(folder);
+                foreach (var e in folders)
+                    Console.WriteLine(e);
+                //выводим файлы, содержащиеся в каталогах
+                Console.WriteLine("Files:");
+                foreach (var e in folders)
+                {
+                    var newFiles = Directory.GetFiles(e);
+                    foreach (var s in newFiles)
+                        Console.WriteLine(s);
+                }
+            }
+        }
+    }
+}
---
+++ Control_Two/SecondNumber/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ControlTask_2
+{
+    // необходимые поля, класс нужной информации
+    class Information
+    {
+        public int postId { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string body { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder();
+            int sum = 0;// сумма букв каждой строки
+            var files = new List<string>();
+            foreach (var json in files)
+            {
+                // десериализируем каждую строку списка в объект 
+                Information information = JsonConvert.DeserializeObject<Information>(json);
+                if (information.id % 2 == 0)// номер должен быть четным
+                    foreach (var e in information.body)
+                        if (e >= 'a' && e <= 'z')
+                            sum++;// считаем только буквы
+                // печатаем для каждой строки ее сумму
+                builder.Append(information.name);
+                builder.Append(" : ");
+                builder.Append(sum);
+                builder.Append("\n");
+                // обнуляем, идем в другую строку
+                sum = 0;
+            }
+            var str = builder.ToString();
+            Console.WriteLine(str);
+        }
+    }
+}
