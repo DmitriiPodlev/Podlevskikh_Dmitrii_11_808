43bd4fd7b15bdf1048258d28ab27eaa4d494df05
DmitriiPodlev <42978023+DmitriiPodlev@users.noreply.github.com>
2021-04-26 16:39:54 +0300

    New model

--- Python_course/MyProject/djangoProject/djangoProject/settings.py
+++ Python_course/MyProject/djangoProject/djangoProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shortUrls'
 ]
 
 MIDDLEWARE = [
@@ -77,7 +79,7 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
     }
 }
 
@@ -106,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
--- Python_course/MyProject/djangoProject/shortUrls/models.py
+++ Python_course/MyProject/djangoProject/shortUrls/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Url(models.Model):
+    short_url = models.CharField(max_length=10)
+    original_url = models.CharField(max_length=500)
+    frequency = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.short_url
