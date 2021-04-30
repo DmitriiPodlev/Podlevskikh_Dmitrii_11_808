613fc152f51d199892ce31e2983ae0c016e7e397
DmitriiPodlev <42978023+DmitriiPodlev@users.noreply.github.com>
2021-04-30 17:13:04 +0300

    Update views

--- Python_course/MyProject/djangoProject/shortUrls/views.py
+++ Python_course/MyProject/djangoProject/shortUrls/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from .models import Url
 
 
-# Create your views here.
 def index(request):
     return HttpResponse('Index page!')
+
+
+def links(request):
+    url_list = Url.objects.order_by('-frequency')
+    context = {
+        'url_list': url_list
+    }
+    return render(request, 'shortUrls/links.html', context)
+
+
+def delete(request, url_id):
+    url = get_object_or_404(Url, pk=url_id)
+    return
